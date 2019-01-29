e1ef76a713be3bed65e3f7d62342148890b0fc34
Tyler Collier <tyler@intraedge.com>
2019-01-28 17:57:01 -0700

    Add solutions for max of array and index of max

--- indexOfMax.test.js
+++ indexOfMax.test.js
@@ -1,5 +1,13 @@
 function getIndexOfMax(arr) {
-  // Your code goes here...
+  let max = arr[0]
+  let indexOfMax = 0
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+      indexOfMax = i
+    }
+  }
+  return indexOfMax
 }
 
 describe('getIndexOfMax', () => {
--- maxOfArray.test.js
+++ maxOfArray.test.js
@@ -1,5 +1,11 @@
 function getMaxOfArray(arr) {
-  // Your code goes here...
+  let max = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+  }
+  return max
 }
 
 describe('getMaxOfArray', () => {
