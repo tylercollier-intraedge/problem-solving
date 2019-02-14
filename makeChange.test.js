cc4fe065692a26c758eac0b2a50cf81c1a258b33
Tyler Collier <tyler@intraedge.com>
2019-02-14 08:55:40 -0700

    Add challenge to make change for a dollar

---
+++ makeChange.test.js
@@ -0,0 +1,4 @@
+// Write a function that will make change for a dollar. You're given an integer representing the
+// cost, which will be between 1 and 99 inclusive, and you should make change using as few coins
+// as possible. Return the change as an array of coins represented by integers. E.g. if you are
+// given the number 85, you would return [10, 5].
